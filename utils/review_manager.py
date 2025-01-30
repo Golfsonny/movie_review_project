ac3640b30270b41a1f200cd30b22398de51071d2
GolfSonny <tradingSon717@gmail.com>
2025-01-30 22:01:27 +0900

    Update review_manager.py
    Minor Update

--- utils/review_manager.py
+++ utils/review_manager.py
@@ -5,6 +5,8 @@ def add_review(reviews):
         rating = int(input("평점 (1-10): "))
         if not (1 <= rating <= 10):
             raise ValueError
+            
+    #범위밖 입력 예외처리
     except ValueError:
         print("평점은 1에서 10 사이의 정수여야 합니다.")
         return
@@ -17,6 +19,7 @@ def delete_review(reviews, title):
     for review in reviews:
         if review["title"].lower() == title.lower():
             reviews.remove(review)
+            #리스트에서 첫 번째로 일치하는 항목을 제거
             print(f"'{title}' 리뷰가 삭제되었습니다.")
             return
     print(f"'{title}' 리뷰를 찾을 수 없습니다.")
